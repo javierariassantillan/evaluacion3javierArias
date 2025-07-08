f940c0b948b2cfce63917945c477689f2c025391
javierariassantillan <javier.arias.santillan@udla.edu.ec>
2025-07-08 07:49:58 -0500

    Se envia prenda.cs

---
+++ evaluacion3javierArias/PredanDatabase.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+
+using evaluacion3javierArias.Models;
+
+
+
+namespace evaluacion3javierArias.Data
+
+{
+
+    public class PrendaDatabase
+
+    {
+
+        private readonly SQLiteAsyncConnection _database;
+
+
+
+        public PrendaDatabase(string dbPath)
+
+        {
+
+            _database = new SQLiteAsyncConnection(dbPath);
+
+            _database.CreateTableAsync<Prenda>().Wait();
+
+        }
+
+
+
+        public Task<List<Prenda>> GetPrendasAsync() => _database.Table<Prenda>().ToListAsync();
+
+        public Task<int> SavePrendaAsync(Prenda item) => _database.InsertAsync(item);
+
+    }
+
+}
---
+++ evaluacion3javierArias/Prenda.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+
+
+
+namespace evaluacion3javierArias.Models
+
+{
+
+    public class Prenda
+
+    {
+
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+
+        public string PrendaNombre { get; set; }
+
+        public string Color { get; set; }
+
+        public int Talla { get; set; }
+
+        public bool EnInventario { get; set; }
+
+    }
+
+}
