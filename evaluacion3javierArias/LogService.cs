2729832b2b2e844f9f3374de688e3056ece716e5
javierariassantillan <javier.arias.santillan@udla.edu.ec>
2025-07-08 07:52:13 -0500

    Se agrega el servicio de LOGS.

---
+++ evaluacion3javierArias/LogService.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+
+
+namespace evaluacion3javierArias.Services
+
+{
+
+    public class LogService
+
+    {
+
+        private readonly string logPath;
+
+
+
+        public LogService(string apellido)
+
+        {
+
+            string fileName = $"Logs_{apellido}.txt";
+
+            logPath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+
+        }
+
+
+
+        public async Task AppendLogAsync(string mensaje)
+
+        {
+
+            var logEntry = $"{mensaje} el {DateTime.Now:dd/MM/yyyy HH:mm}\n";
+
+            await File.AppendAllTextAsync(logPath, logEntry, Encoding.UTF8);
+
+        }
+
+
+
+        public async Task<string> ReadLogsAsync()
+
+        {
+
+            return File.Exists(logPath) ? await File.ReadAllTextAsync(logPath) : "No hay logs aún.";
+
+        }
+
+    }
+
+}
